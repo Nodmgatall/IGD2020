d9acdf5c2129047912a83bed0606b54100105fd1
Oliver Heidmann <oliver.heidmann@mpimet.mpg.de>
2020-05-03 19:30:07 +0100

    added script for world generation and for camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public float m_speed;
+    public float m_zoomSpeed;
+    public float m_rotationSpeed;
+    public float m_maxHeight = 100;
+    public float m_minHeight = 20;
+    public float boundaryFractionX = 0.015f;
+    public float boundaryFractionY = 0.015f;
+    float boundaryX;
+    float boundaryY;
+
+    private int theScreenWidth;
+    private int theScreenHeight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theScreenWidth = Screen.width;
+        theScreenHeight = Screen.height;
+        boundaryX = theScreenWidth * boundaryFractionX;
+        boundaryY = theScreenHeight * boundaryFractionY;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float currentHeight = transform.position.y;
+        float speed = m_speed * Time.deltaTime *  (currentHeight / 10);
+        Vector3 forward = speed * transform.forward;
+        Vector3 side = speed * transform.right;
+        Vector3 change = new Vector3(0,0,0);
+
+        if(Input.GetKey("w")){change += forward;}
+        if(Input.GetKey("s")){change -= forward;}
+        if(Input.GetKey("d")){change += side;}
+        if(Input.GetKey("a")){change -= side;}
+
+        if(Input.GetKey("q")){ transform.Rotate(new Vector3(0,m_rotationSpeed,0) * -1 * Time.deltaTime);}
+        if(Input.GetKey("e")){ transform.Rotate(new Vector3(0,m_rotationSpeed,0)      * Time.deltaTime);}
+
+        float mW = Input.GetAxis("Mouse ScrollWheel");
+        if(mW  != 0){
+            float newHeight = Mathf.Max(transform.position.y - (mW * m_zoomSpeed), m_minHeight);
+            transform.position = new Vector3(transform.position.x,Mathf.Min(newHeight, m_maxHeight),transform.position.z);
+            Debug.Log("what now?");
+        }
+        float mPosX =Input.mousePosition.x;
+        float mPosY =Input.mousePosition.y;
+        if ( mPosX > theScreenWidth - boundaryX && mPosX < theScreenWidth)
+        {
+            change.x += speed;
+        }
+
+        if (mPosX < 0 + boundaryX && mPosX > 0)
+        {
+            change.x -= speed;
+        }
+
+        if (mPosY > theScreenHeight - boundaryY && mPosY < theScreenHeight)
+        {
+            change.z += speed;
+        }
+
+        if (mPosY < 0 + boundaryY && mPosY > 0)
+        {
+            change.z -= speed;
+        }
+
+        transform.position += change;
+    }
+}
--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System;
+using System.Linq;
 using System.Collections;
 
 public struct MapGrid
@@ -8,11 +9,13 @@ public struct MapGrid
     Vector3 offsetTopDown;
     Vector3 offestTopLeft;
     int m_radius;
+    public float maxY;
     public Vector3[] grid;
 
     int numHexTiles;
 
-    public MapGrid(Vector3 p_origin, int p_radius){
+    public MapGrid(Vector3 p_origin, int p_radius)
+    {
         m_radius = p_radius;
         p_radius = p_radius + 1;
         origin = p_origin;
@@ -21,8 +24,8 @@ public struct MapGrid
         offsetTopDown= new Vector3(0f,0f,10.003f);
         offestTopLeft= new Vector3(8.6625f,0f, -5.0f);
         numHexTiles = (3 * (p_radius * p_radius)) - (3 * p_radius) + 1;
-        Debug.Log(numHexTiles);
         grid = new Vector3[numHexTiles];
+        maxY = m_radius * offsetTopDown.z;
     }
 
     public void createHexShapedBasicGrid()
@@ -35,7 +38,7 @@ public struct MapGrid
             int r2 =Math.Min(p_radius, -q + p_radius);
             for (int r = r1; r <= r2; r++) {
                 grid[tileID] = origin + q * offestTopLeft + r * offsetTopDown + r * offestTopLeft;
-                Debug.Log(tileID);
+                maxY= Math.Max(maxY, grid[tileID].z);
                 tileID++;
             }
         }
@@ -46,16 +49,122 @@ public class WorldGen : MonoBehaviour
 {
     // Start is called before the first frame update
     MapGrid map_grid;
-    public GameObject myPrefab;
-    public int radius;
+
+    public GameObject[] noiseGens = new GameObject[6];
+    public GameObject[] prefabs = new GameObject[6];
+    GameObject map;
+
+    public GameObject elevationNoise;
+    public AnimationCurve climate;
+
+    float[] noiseVals;
+
+    float maxYY = 0;
+    public int radius = 12;
     public Vector3 origin;
-    void Start()
+    void Start(){
+        map = new GameObject();
+        map.transform.parent  = transform;
+    }
+    public void LoadMap()
     {
+        Debug.Log("LoadMap called");
+        foreach (Transform child in map.transform)
+        {
+            Destroy(child.gameObject);
+        }
         map_grid = new MapGrid( origin, radius);
         map_grid.createHexShapedBasicGrid();
-        foreach (Vector3 x in map_grid.grid){
-            Debug.Log(x);
-            Instantiate(myPrefab,x, Quaternion.identity);
+        foreach (Vector3 v in map_grid.grid){
+            float x = v.x;
+            float y = v.z;
+            float[] noiseVals = getNoise(x, y, origin);
+            int tileIDx = getTileIDXFromNoise(noiseVals,x,y);
+            var obj = Instantiate(getPrefab(tileIDx),v, Quaternion.identity);
+            obj.transform.parent = map.transform;
+
         }
+    }
+    float[] getNoise(float x, float y, Vector3 chunkOrigin){
+        float[] localNoiseVals = new float[noiseGens.Length];
+        for (int i = 0; i < noiseGens.Length; i++)
+        {
+            if (noiseGens[i] != null){
+                perlinNoise p = noiseGens[i].GetComponent(typeof(perlinNoise)) as perlinNoise;
+                localNoiseVals[i] = p.getNoise(x,y, chunkOrigin);
+            }
+        }
+        return localNoiseVals;
+
+    }
+
+    bool printOnce = true;
+    int getTileIDXFromNoise(float [] noiseValues,float x, float y)
+    {
+        int retVal =  0;
+        perlinNoise heightNoise = elevationNoise.GetComponent(typeof(perlinNoise)) as perlinNoise;
+        float heigtAtXY = heightNoise.getNoise(x,y,origin);
+        float toEval = Math.Abs(y /(map_grid.maxY));
+        float climateValues = climate.Evaluate(toEval);
+        maxYY =Math.Max( climateValues, maxYY);
+        if(printOnce)
+        {
+            Debug.Log(toEval);
+            Debug.Log(x);
+            Debug.Log(y);
+            Debug.Log(map_grid.maxY);
+            printOnce = false;
+        }
+        if(climateValues > 1 || climateValues < 0)
+        {
+            Debug.Log("ERROR: VALUES NOT IN EXPECTED RANGE");
+            Debug.Log(climateValues);
+        }
+        if(climateValues > 0.95)
+        {
+            //return "sand"
+            retVal =  4;
+        }
+        else if(climateValues > 0.7)
+        {
+            //retVal =  Grass
+            retVal =  5;
+        }
+        else if(climateValues > 0.55)
+        {
+            //retVal =  Forest
+            retVal =  1;
+        }
+        else if(climateValues > 0.3)
+        {
+            //retVal =  Grass
+            retVal =  5;
+        }
+        if(retVal != 0)
+        {
+            //retVal = noiseValues.ToList().IndexOf(noiseValues.Max());
+            if(noiseValues[1] > 0.0f)
+            {
+                retVal = 2;
+            }
+            if(noiseValues[2] > 0.0f)
+            {
+                retVal = 3;
+            }
+            if(noiseValues[0] > 0.0f)
+            {
+                retVal = 0;
+            }
+
+        }
+        return retVal;
+    }
+
+    GameObject getPrefab(int val)
+    {
+        if(prefabs[val] != null)
+            return prefabs[val];
+        Debug.Log("Error: prefab not set");
+        return prefabs[0];
     }
 }
